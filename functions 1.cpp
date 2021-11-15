7c34d67ce8c2d4c75992b3c6ccd891d9af224bfb
Nashon Ernest <92583323+ernestnash@users.noreply.github.com>
2021-11-15 13:04:55 +0300

    Update functions 1.cpp

--- functions 1.cpp
+++ functions 1.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
 	int total(int,int);
 	int subj1,subj2,tot;
+	
 	cout<<"enter subject 1  ";
 	cin>>subj1;
+	
 	cout<<"enter subject 2  ";
 	cin>>subj2;
+	
 	tot=total(subj1,subj2);
+	
 	cout<<"total is  "<<tot;
+	
 	return 0;
 }
 int total(int subj1,int subj2)
